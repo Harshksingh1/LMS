b9d053abad5ed714921e380e34be9e3edf988ef9
Harsh Singh <117902266+Harshksingh1@users.noreply.github.com>
2025-11-10 01:02:15 +0530

    Restrict CORS origin to specific URL

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:true,
+    origin:"https://lms-1-n4px.onrender.com",
     credentials:true
 }))
 app.use("/api/auth", authRouter)
